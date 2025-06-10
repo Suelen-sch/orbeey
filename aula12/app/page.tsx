1ef6e6865c8402c991e1e54de66e099d66ccdafd
Suelen-sch <Suschmorantz@gmail.com>
2025-06-10 19:49:40 -0300

    feat add card's na home

--- aula12/app/page.tsx
+++ aula12/app/page.tsx
@@ -43,7 +43,7 @@ export default function home() {
         <div className={styles.cardsContainer}>
           <div className={styles.card}>
             <h3>Orbeey Technology</h3>
-            <p>
+            <p className={styles.descriptionCard}>
               Nossa equipe de especialistas em tecnologia oferece soluções personalizadas para otimizar seus
               processos e inovar seus produtos. Desde o desenvolvimento de software até consultoria em TI,
               estamos prontos para transformar sua visão em realidade.
@@ -53,7 +53,7 @@ export default function home() {
 
           <div className={styles.card}>
             <h3>Orbeey Finance</h3>
-            <p>
+            <p className={styles.descriptionCard}>
               Oferecemos serviços completos de consultoria financeira, ajudando sua empresa a planejar,
               gerenciar e crescer de maneira sustentável. Nossos consultores são experientes em planejamento
               financeiro, gestão de investimentos e estratégias fiscais.
@@ -63,7 +63,7 @@ export default function home() {
 
           <div className={styles.card}>
             <h3>Orbeey Coin</h3>
-            <p>
+            <p className={styles.descriptionCard}>
               Especialistas em criptomoedas e blockchain, fornecemos consultoria e soluções para sua empresa
               adotar essa tecnologia inovadora. Desde a implementação até a gestão de ativos digitais, nossa equipe
               está pronta para orientar você em cada etapa.
@@ -71,6 +71,29 @@ export default function home() {
             <button className={styles.cardButton}>➤ Saiba mais</button>
           </div>
         </div>
+
+
+        <div className={styles.cardsContainer}>
+          <div className={styles.card}>
+            <h3>Orbeey Design</h3>
+            <p className={styles.descriptionCard}>Transforme sua marca com nossos serviços de design gráfico e web. Criamos identidades visuais impactantes e sites funcionais que capturam a essência do seu negócio. Nossos projetos são personalizados para atender às suas necessidades específicas.</p>
+            <button className={styles.cardButton}>➤ Saiba mais</button>
+          </div>
+
+          <div className={styles.card}>
+            <h3>Orbeey Contability</h3>
+            <p className={styles.descriptionCard}>Com ampla experiência em contabilidade empresarial, oferecemos serviços de gestão fiscal e contábil para garantir que sua empresa esteja sempre em conformidade e bem gerida. Nossa equipe está pronta para ajudar você a alcançar a eficiência financeira.</p>
+            <button className={styles.cardButton}>➤ Saiba mais</button>
+          </div>
+
+          <div className={styles.card}>
+            <h3>Orbeey Law</h3>
+            <p className={styles.descriptionCard}>Nossa missão é fornecer serviços jurídicos de alta qualidade, com uma equipe de advogados especializados prontos para atender suas necessidades específicas. Explore nossas áreas de atuação e descubra como podemos ajudar você.</p>
+            <button className={styles.cardButton}>➤ Saiba mais</button>
+          </div>
+        </div>
+
+
       </section>
 
       <style jsx global>{`
